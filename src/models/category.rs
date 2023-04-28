ed5634be388b2053e849233027e4341b1917d28d
bujosa <davidbujosa@gmail.com>
2023-04-28 19:27:56 -0400

    fix(models): make some properties optional in category struct

--- src/models/category.rs
+++ src/models/category.rs
@@ -3,13 +3,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CategoryMarketDataResponse {
     id: String,
-    name: String,
-    market_cap: f64,
-    market_cap_change_24h: f64,
-    content: String,
+    name: Option<String>,
+    market_cap: Option<f64>,
+    market_cap_change_24h: Option<f64>,
+    content: Option<String>,
     top_3_coins: Vec<String>,
-    volume_24h: f64,
-    updated_at: String,
+    volume_24h: Option<f64>,
+    updated_at: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
